13388f5278c0271156d04ff70631ccc1c7cb5a38
TTOExreme <camarottol@hotmail.com>
2018-01-01 20:07:58 -0200

    Y18D001

--- GitUP.sh
+++ GitUP.sh
@@ -9,7 +9,7 @@ git commit -m ${DATE}
 git checkout -b ${DATE}
 git checkout ${DATE}
 git remote add origin "https://github.com/TTOExtreme/EveryApp.git"
-git push -u origin master
+git push -u origin ${DATE}
 
 echo "finished"
 sleep 3
