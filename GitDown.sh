1c32d8f8bed451850aaf84d7bffbfeedac453ebb
TTOExtreme <camarottol@hotmail.com>
2018-01-02 12:07:27 -0200

    Y18D002T1207

--- GitDown.sh
+++ GitDown.sh
@@ -3,13 +3,7 @@ clear
 echo "Uploading to Database"
 DATE=Y`date '+%g'`D`date '+%j'`T`date '+%H%M'`
 
-git init
-git add alpha-v3/ .
-git commit -m ${DATE}
-git checkout -b ${DATE}
-git checkout ${DATE}
-git remote add origin "https://github.com/TTOExtreme/EveryApp.git"
-git push -u origin master
+git clone https://github.com/TTOExtreme/EveryApp.git /
 
 echo "finished"
 sleep 3
